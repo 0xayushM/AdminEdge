17d4711c6735af1b4585904ed529f9193c2da94a
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-11-25 02:34:45 +0530

    ./Button issue

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-cycle
-export { default as Button } from './Button';
+// export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
 export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
